811e1898f5b31ebf37582ebde52043f760ce2f09
Aaron Geller <a-geller@northwestern.edu>
2022-04-21 15:11:14 -0500

    added ability to switch shaders in gui

--- src/js/render.js
+++ src/js/render.js
@@ -11,6 +11,8 @@ function defineParams(){
 
 		this.useShader = 2;
 
+		this.volume = {};
+
 		this.container = null;
 		this.renderer = null;
 		this.scene = null;
@@ -26,14 +28,19 @@ function defineParams(){
 		this.cmtextures = null
 		this.volumeMesh = null;
 
-		if (this.useShader == 1){
-			// better for 128 FIRE gridding
-			this.volconfig = {clim1: 0.3, clim2: 0.8, renderstyle: 'mip', isothreshold: 0.5, colormap: 'viridis' };
-			// better for 256 FIRE gridding
-			// this.volconfig = {clim1: 0.2, clim2: 0.6, renderstyle: 'mip', isothreshold: 0.5, colormap: 'viridis' };
-		} else {
-			this.volconfig = {threshold: 0.6, range: 0.1, opacity: 0.25, steps: 100, base: {r:0, g:0, b:0} };
-		}
+		this.renderstyle = 'volumetric'; //'mip', 'iso'
+
+		this.volconfig = [];
+
+		// shader 1
+		// better for 128 FIRE gridding
+		this.volconfig.push({clim1: 0.3, clim2: 0.8, isothreshold: 0.5, colormap: 'viridis' });
+		// better for 256 FIRE gridding
+		// this.volconfig = {clim1: 0.2, clim2: 0.6, renderstyle: 'mip', isothreshold: 0.5, colormap: 'viridis' };
+		
+		// shader 2
+		this.volconfig.push({threshold: 0.6, range: 0.1, opacity: 0.25, steps: 100, base: {r:0, g:0, b:0} });
+		
 
 
 	};
@@ -42,7 +49,7 @@ function defineParams(){
 }
 
 // this initializes everything needed for the scene
-function init(volume){
+function init(){
 
 	var screenWidth = window.innerWidth;
 	var screenHeight = window.innerHeight;
@@ -52,20 +59,30 @@ function init(volume){
 	params.renderer = new THREE.WebGLRenderer( {
 		antialias:true,
 	} );
-	params.renderer.setSize(screenWidth, screenHeight);
+	params.renderer.setSize(window.innerWidth, window.innerHeight);
 
 	params.container = document.getElementById('WebGLContainer');
 	params.container.appendChild( params.renderer.domElement );
 
 	// scene
 	params.scene = new THREE.Scene();     
+}
+
+function reinit(){
+
+	// clear the scene
+	while (params.scene.children.length){
+    	params.scene.remove(params.scene.children[0]);
+	}
+
+	var aspect = window.innerWidth/window.innerHeight;
 
 	if (params.useShader == 1){
 		// Create camera (The volume renderer in shader 1 does not work very well with perspective yet)
 		const h = 256; // frustum height (not sure what to use here)
 		//params.camera = new THREE.OrthographicCamera( -h*aspect/2, h*aspect/2, h/2, -h/2, params.zmin, params.zmax);
 		params.camera = new THREE.OrthographicCamera( -h*aspect/2, h*aspect/2, h/2., -h/2, params.zmin, params.zmax);
-		params.camera.position.set( volume.size[0]/2., volume.size[1]/2., volume.size[2] );
+		params.camera.position.set( params.volume.size[0]/2., params.volume.size[1]/2., params.volume.size[2] );
 	} else {
 		params.camera = new THREE.PerspectiveCamera( params.fov, aspect, params.zmin, params.zmax );
 		params.camera.position.set( 1.5, 1.5, 1.5 );
@@ -97,25 +114,51 @@ function onWindowResize() {
 
 }
 
+function createUIStatic(){
+	var gui = new dat.GUI({ autoPlace: false });
+
+	var container = d3.select('body').append('div')
+		.attr('id','guiContainer')
+		.style('position', 'absolute')
+		.style('top','2px')
+		.style('left','2px');
+	container.node().appendChild(gui.domElement);
+
+
+	gui.add(params,'renderstyle',{volumetric: 'volumetric', mip: 'mip', iso: 'iso' } ).onChange( updateRenderStyle );
+}
+
+function updateRenderStyle(){
+	var currentShader = params.useShader;
+	console.log(params.renderstyle)
+	if (params.renderstyle == 'mip' || params.renderstyle == 'iso'){
+		params.useShader = 1;
+		if (currentShader == params.useShader) params.volumeMesh.material.uniforms['u_renderstyle'].value = params.volconfig[0].renderstyle == 'mip' ? 0 : 1; // 0: MIP, 1: ISO
+	} else {
+		params.useShader = 2;
+	}
+
+	if (currentShader != params.useShader) WebGLStart();
+}
 ////////////////
 // For VolumeRenderShader1
 // this creates the user interface (gui)
 function createUI1(){
 
+	if (params.gui) params.gui.destroy();
+
 	params.gui = new dat.GUI();
-	params.gui.add( params.volconfig, 'clim1', 0, 1, 0.01 ).onChange( updateUniforms1 );
-	params.gui.add( params.volconfig, 'clim2', 0, 1, 0.01 ).onChange( updateUniforms1 );
-	params.gui.add( params.volconfig, 'colormap', { gray: 'gray', viridis: 'viridis' } ).onChange( updateUniforms1 );
-	params.gui.add( params.volconfig, 'renderstyle', { mip: 'mip', iso: 'iso' } ).onChange( updateUniforms1 );
-	params.gui.add( params.volconfig, 'isothreshold', 0, 1, 0.01).onChange( updateUniforms1 );
+	params.gui.add( params.volconfig[0], 'clim1', 0, 1, 0.01 ).onChange( updateUniforms1 );
+	params.gui.add( params.volconfig[0], 'clim2', 0, 1, 0.01 ).onChange( updateUniforms1 );
+	params.gui.add( params.volconfig[0], 'colormap', { gray: 'gray', viridis: 'viridis' } ).onChange( updateUniforms1 );
+	params.gui.add( params.volconfig[0], 'isothreshold', 0, 1, 0.01).onChange( updateUniforms1 );
 
 }
 function updateUniforms1() {
 
-	params.volumeMesh.material.uniforms[ 'u_clim' ].value.set( params.volconfig.clim1, params.volconfig.clim2 );
-	params.volumeMesh.material.uniforms[ 'u_renderstyle' ].value = params.volconfig.renderstyle == 'mip' ? 0 : 1; // 0: MIP, 1: ISO
-	params.volumeMesh.material.uniforms[ 'u_renderthreshold' ].value = params.volconfig.isothreshold; // For ISO renderstyle
-	params.volumeMesh.material.uniforms[ 'u_cmdata' ].value = params.cmtextures[ params.volconfig.colormap ];
+	params.volumeMesh.material.uniforms[ 'u_clim' ].value.set( params.volconfig[0].clim1, params.volconfig[0].clim2 );
+	params.volumeMesh.material.uniforms[ 'u_renderthreshold' ].value = params.volconfig[0].isothreshold; // For ISO renderstyle
+	params.volumeMesh.material.uniforms[ 'u_cmdata' ].value = params.cmtextures[ params.volconfig[0].colormap ];
 
 	render();
 
@@ -123,11 +166,11 @@ function updateUniforms1() {
 
 
 // this will draw the scene using the first type of shader
-function drawScene1(volume){
+function drawScene1(){
 
-	console.log(volume)
+	console.log(params.volume)
 
-	const texture = new THREE.Data3DTexture(volume.data, volume.size[0], volume.size[1], volume.size[2]);
+	const texture = new THREE.Data3DTexture(params.volume.data, params.volume.size[0], params.volume.size[1], params.volume.size[2]);
 	texture.format = THREE.RedFormat;
 	texture.type = THREE.FloatType;
 	texture.minFilter = texture.magFilter = THREE.LinearFilter;
@@ -146,11 +189,11 @@ function drawScene1(volume){
 	const uniforms = THREE.UniformsUtils.clone( shader.uniforms );
 
 	uniforms[ 'u_data' ].value = texture;
-	uniforms[ 'u_size' ].value.set(volume.size[0], volume.size[1], volume.size[2]);
-	uniforms[ 'u_clim' ].value.set(params.volconfig.clim1, params.volconfig.clim2);
-	uniforms[ 'u_renderstyle' ].value = params.volconfig.renderstyle == 'mip' ? 0 : 1; // 0: MIP, 1: ISO
-	uniforms[ 'u_renderthreshold' ].value = params.volconfig.isothreshold; // For ISO renderstyle
-	uniforms[ 'u_cmdata' ].value = params.cmtextures[params.volconfig.colormap];
+	uniforms[ 'u_size' ].value.set(params.volume.size[0], params.volume.size[1], params.volume.size[2]);
+	uniforms[ 'u_clim' ].value.set(params.volconfig[0].clim1, params.volconfig[0].clim2);
+	uniforms[ 'u_renderstyle' ].value = params.renderstyle == 'mip' ? 0 : 1; // 0: MIP, 1: ISO
+	uniforms[ 'u_renderthreshold' ].value = params.volconfig[0].isothreshold; // For ISO renderstyle
+	uniforms[ 'u_cmdata' ].value = params.cmtextures[params.volconfig[0].colormap];
 
 	const material = new THREE.ShaderMaterial( {
 		uniforms: uniforms,
@@ -160,11 +203,11 @@ function drawScene1(volume){
 	} );
 
 	// THREE.Mesh
-	const geometry = new THREE.BoxGeometry(volume.size[0], volume.size[1], volume.size[2] );
-	geometry.translate(volume.size[0]/2 - 0.5, volume.size[1]/2 - 0.5, volume.size[2]/2 - 0.5);
+	const geometry = new THREE.BoxGeometry(params.volume.size[0], params.volume.size[1], params.volume.size[2] );
+	geometry.translate(params.volume.size[0]/2 - 0.5, params.volume.size[1]/2 - 0.5, params.volume.size[2]/2 - 0.5);
 
 	params.volumeMesh = new THREE.Mesh(geometry, material);
-	params.volumeMesh.position.set(-(volume.size[0]/2 - 0.5), -(volume.size[1]/2 - 0.5), -(volume.size[2]/2 - 0.5));
+	params.volumeMesh.position.set(-(params.volume.size[0]/2 - 0.5), -(params.volume.size[1]/2 - 0.5), -(params.volume.size[2]/2 - 0.5));
 	params.scene.add( params.volumeMesh );
 
 }
@@ -174,21 +217,23 @@ function drawScene1(volume){
 // this creates the user interface (gui)
 function createUI2(){
 
+	if (params.gui) params.gui.destroy();
+
 	params.gui = new dat.GUI();
-	params.gui.add( params.volconfig, 'threshold', 0, 1, 0.01 ).onChange( updateUniforms2 );
-	params.gui.add( params.volconfig, 'opacity', 0, 1, 0.01 ).onChange( updateUniforms2 );
-	params.gui.add( params.volconfig, 'range', 0, 1, 0.01 ).onChange( updateUniforms2 );
-	params.gui.add( params.volconfig, 'steps', 0, 200, 1 ).onChange( updateUniforms2 );
-	params.gui.addColor( params.volconfig, 'base').onChange( updateUniforms2 );
+	params.gui.add( params.volconfig[1], 'threshold', 0, 1, 0.01 ).onChange( updateUniforms2 );
+	params.gui.add( params.volconfig[1], 'opacity', 0, 1, 0.01 ).onChange( updateUniforms2 );
+	params.gui.add( params.volconfig[1], 'range', 0, 1, 0.01 ).onChange( updateUniforms2 );
+	params.gui.add( params.volconfig[1], 'steps', 0, 200, 1 ).onChange( updateUniforms2 );
+	params.gui.addColor( params.volconfig[1], 'base').onChange( updateUniforms2 );
 
 }
 function updateUniforms2() {
 
-	params.volumeMesh.material.uniforms[ 'u_threshold' ].value = params.volconfig.threshold;
-	params.volumeMesh.material.uniforms[ 'u_range' ].value = params.volconfig.range;
-	params.volumeMesh.material.uniforms[ 'u_opacity' ].value = params.volconfig.opacity;
-	params.volumeMesh.material.uniforms[ 'u_steps' ].value = params.volconfig.steps; 
-	params.volumeMesh.material.uniforms[ 'u_base' ].value.set(params.volconfig.base.r/255., params.volconfig.base.g/255., params.volconfig.base.b/255.); 
+	params.volumeMesh.material.uniforms[ 'u_threshold' ].value = params.volconfig[1].threshold;
+	params.volumeMesh.material.uniforms[ 'u_range' ].value = params.volconfig[1].range;
+	params.volumeMesh.material.uniforms[ 'u_opacity' ].value = params.volconfig[1].opacity;
+	params.volumeMesh.material.uniforms[ 'u_steps' ].value = params.volconfig[1].steps; 
+	params.volumeMesh.material.uniforms[ 'u_base' ].value.set(params.volconfig[1].base.r/255., params.volconfig[1].base.g/255., params.volconfig[1].base.b/255.); 
 
 	render();
 
@@ -196,11 +241,11 @@ function updateUniforms2() {
 
 
 // this will draw the scene using the first type of shader
-function drawScene2(volume){
+function drawScene2(){
 
-	console.log(volume)
+	console.log(params.volume)
 
-	const texture = new THREE.Data3DTexture(volume.data, volume.size[0], volume.size[1], volume.size[2]);
+	const texture = new THREE.Data3DTexture(params.volume.data, params.volume.size[0], params.volume.size[1], params.volume.size[2]);
 	texture.format = THREE.RedFormat;
 	texture.type = THREE.FloatType;
 	texture.minFilter = texture.magFilter = THREE.LinearFilter;
@@ -219,11 +264,11 @@ function drawScene2(volume){
 	const uniforms = THREE.UniformsUtils.clone( shader.uniforms );
 
 	uniforms[ 'u_data' ].value = texture;
-	uniforms[ 'u_threshold' ].value = params.volconfig.threshold;
-	uniforms[ 'u_range' ].value = params.volconfig.range;
-	uniforms[ 'u_opacity' ].value = params.volconfig.opacity;
-	uniforms[ 'u_steps' ].value = params.volconfig.steps; 
-	uniforms[ 'u_base' ].value.set(params.volconfig.base.r/255., params.volconfig.base.g/255., params.volconfig.base.b/255.); 
+	uniforms[ 'u_threshold' ].value = params.volconfig[1].threshold;
+	uniforms[ 'u_range' ].value = params.volconfig[1].range;
+	uniforms[ 'u_opacity' ].value = params.volconfig[1].opacity;
+	uniforms[ 'u_steps' ].value = params.volconfig[1].steps; 
+	uniforms[ 'u_base' ].value.set(params.volconfig[1].base.r/255., params.volconfig[1].base.g/255., params.volconfig[1].base.b/255.); 
 	uniforms[ 'u_cameraPos' ].value.copy(params.camera.position); 
 
 	const material = new THREE.ShaderMaterial( {
@@ -234,14 +279,15 @@ function drawScene2(volume){
 	} );
 
 	// THREE.Mesh
-	const geometry = new THREE.BoxGeometry(volume.size[0], volume.size[1], volume.size[2] );
-	geometry.translate(volume.size[0]/2 - 0.5, volume.size[1]/2 - 0.5, volume.size[2]/2 - 0.5);
+	const geometry = new THREE.BoxGeometry(params.volume.size[0], params.volume.size[1], params.volume.size[2] );
+	geometry.translate(params.volume.size[0]/2 - 0.5, params.volume.size[1]/2 - 0.5, params.volume.size[2]/2 - 0.5);
 
 	params.volumeMesh = new THREE.Mesh(geometry, material);
-	//params.volumeMesh.position.set(-(volume.size[0]/2 - 0.5), -(volume.size[1]/2 - 0.5), -(volume.size[2]/2 - 0.5));
+	//params.volumeMesh.position.set(-(params.volume.size[0]/2 - 0.5), -(params.volume.size[1]/2 - 0.5), -(params.volume.size[2]/2 - 0.5));
 	params.scene.add( params.volumeMesh );
 
 }
+
 // this is the animation loop
 function animate(time) {
 	requestAnimationFrame( animate );
@@ -260,23 +306,23 @@ function render() {
 }
 
 // this is called to start everything
-function WebGLStart(volume){
+function WebGLStart(){
 
 // initialize the scence
-	init(volume);
+	reinit();
 
 	if (params.useShader == 1){
 	// create the UI
 		createUI1();
 
 	// draw everything
-		drawScene1(volume);
+		drawScene1();
 	} else {
 	// create the UI
 		createUI2();
 
 	// draw everything
-		drawScene2(volume);
+		drawScene2();
 	}
 
 // start the animation loop
@@ -292,22 +338,22 @@ Promise.all([
 	// d3.csv('src/data/data3Dsizes.csv'),
 ]).then(function(d) {
 	defineParams();
-
-	const volume = {};
+	createUIStatic();
+	init();
 
 	//the volume shader requires a Float34Array for the data
-	volume.data = new Float32Array(d[0].length);
+	params.volume.data = new Float32Array(d[0].length);
 	d[0].forEach(function(dd,i){
-		volume.data[i] = +dd.data;
+		params.volume.data[i] = +dd.data;
 	})
-	volume.size = [
+	params.volume.size = [
 		parseInt(d[1][0].xLength),
 		parseInt(d[1][0].yLength),
 		parseInt(d[1][0].zLength)
 	]
 
 
-	WebGLStart(volume);
+	WebGLStart();
 })
 .catch(function(error){
 	console.log('ERROR:', error)
